d15a4f755a12d2ea3dfc62d02fadf2c14bc1a9ee
adllolokbye <adllistmanagement@gmail.com>
2026-07-31 21:05:18 +1000

    Update List.js

--- js/pages/List.js
+++ js/pages/List.js
@@ -51,8 +51,8 @@ export default {
                             <p>{{ level.id }}</p>
                         </li>
                         <li>
-                            <div class="type-title-sm">Password</div>
-                            <p>{{ level.password || 'Free to Copy' }}</p>
+                            <div class="type-title-sm">Level Length</div>
+                            <p>{{ level.length || 'Free to Copy' }}</p>
                         </li>
                     </ul>
                     <h2>Records</h2>
