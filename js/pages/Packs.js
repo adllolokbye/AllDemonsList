14342fb4e199af82a26e59a22fd767946a026228
adllolokbye <adllistmanagement@gmail.com>
2026-08-02 00:24:16 +1000

    Update Packs.js

--- js/pages/Packs.js
+++ js/pages/Packs.js
@@ -1,1 +1,28 @@
 
+import Spinner from "../components/Spinner.js";
+
+export default {
+    components: { Spinner },
+    template: `
+        <main v-if="loading">
+            <Spinner></Spinner>
+        </main>
+        <main v-else class="page-packs">
+            <div class="packs-container">
+                <table class="packs" v-if="packs">
+                    <tr v-for="([level, err], i) in pack">
+                        <td class="soon">
+                            <p>Coming Soon!</p>
+                        </td>
+                    </tr>
+                </table>
+            </div>
+            <div class="comingsoon-container">
+                <div class="pack">
+                   <h1>
+                   Coming Soon!</h1>
+                </div>
+            </div>
+      </main>
+  `,
+}
